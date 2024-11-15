7001033ec590b188650aa039c3360d9122ea93ec
bandish1304 <151697802+bandish1304@users.noreply.github.com>
2024-11-15 06:49:47 -0800

    Time: 25 ms (36.51%), Space: 16.8 MB (50.99%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = {}
+        for c in magazine:
+            if c in counter:
+                counter[c] += 1
+            else:
+                counter[c] = 1
+                
+        for c in ransomNote:
+            if c not in counter:
+                return False
+            elif counter[c] == 1:
+                del counter[c]
+            else:
+                counter[c] -= 1
+                
+        return True
+    
+    # Time complexity: O(m+n)
+    # Space complexity: O(1)
+                
+    
+                    
+                
+        
